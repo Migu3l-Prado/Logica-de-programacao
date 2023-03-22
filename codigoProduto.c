ca5a00da70a09739774c6e2f90b5ea9502c4acda
Miguel Prado de Sousa <124407704+Migu3l-Prado@users.noreply.github.com>
2023-03-22 19:50:50 -0300

    Update codigoProduto.c

--- codigoProduto.c
+++ codigoProduto.c
@@ -4,17 +4,17 @@ main(){
 	setlocale(LC_ALL,"Portuguese");
 	int codigo;
 	
-	printf("Digite o cÛdigo do produto: \n");
+	printf("Digite o c√≥digo do produto [1-15] \n");
 	scanf("%d",&codigo);
 	
 	switch(codigo){
 	
-	case 1: printf("Alimento n„o perecivel");break;
+	case 1: printf("Alimento n√£o perecivel");break;
 	case 2: case 3: case 4: printf("Alimento perecivel");break;
-	case 5: case 6:printf("Vestu·rio");break;
+	case 5: case 6:printf("Vestu√°rio");break;
 	case 7: printf("Higiene pessoal");break;
-	case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: printf("Limpeza e utensÌlios domÈsticos");break;
-	default: printf("inv·lido");
+	case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: printf("Limpeza e utens√≠lios dom√©sticos");break;
+	default: printf("inv√°lido");
 	
 	
 }
